dc074db7e84320a53d4897d45dc6ed45d828a6c4
unknown <saliou barry>
2021-09-12 17:29:56 +0200

    e

---
+++ feestlunch.py
@@ -0,0 +1,8 @@
+croissantjes = 17
+prijs = 0.39
+stokbroden = 2
+prijs = 2.78
+kortingsbonnen = 3
+perBon = 0.50
+
+print(17*0.39+2*2.78-3*0.50)
---
+++ speelhaldag.py
@@ -0,0 +1,8 @@
+toegangsticket = 7.45
+personen = 4
+aantalGameseatMinuten = 45
+kostenPerMinuten = 5
+prijsPerMinuten = 0.37
+personen = 4
+
+print(7.45*4+45/5+0.37*4)
